30f644cbad9a03f8a53a0142ea7b065cba7a926b
xiunian <nniferyy@gmail.com>
2024-08-07 15:29:25 +0000

    Finish part3

--- kernel/ipc/connection.c
+++ kernel/ipc/connection.c
@@ -68,6 +68,7 @@
  * registration), and
  * @destructor (one routine invoked when some connnection is closed).
  */
+// xiunian: the caller is server
 static int register_server(struct thread *server, unsigned long ipc_routine,
                            cap_t register_thread_cap, unsigned long destructor)
 {
@@ -98,7 +99,7 @@ static int register_server(struct thread *server, unsigned long ipc_routine,
                 return -EINVAL;
         }
 
-        config = kmalloc(sizeof(*config));
+        config = kmalloc(sizeof(*config));      // ipc_server_config
 
         /*
          * @ipc_routine will be the real ipc_routine_entry.
@@ -108,10 +109,9 @@ static int register_server(struct thread *server, unsigned long ipc_routine,
         /* LAB 4 TODO BEGIN (exercise 7) */
         /* Complete the config structure, replace xxx with actual values */
         /* Record the ipc_routine_entry  */
-        // config->declared_ipc_routine_entry = xxx;
-
+        config->declared_ipc_routine_entry = ipc_routine;
         /* Record the registration cb thread */
-        // config->register_cb_thread = xxx;
+        config->register_cb_thread = register_cb_thread;
         /* LAB 4 TODO END (exercise 7) */
 
         register_cb_config = kmalloc(sizeof(*register_cb_config));
@@ -250,10 +250,10 @@ static int create_connection(struct thread *client, struct thread *server,
 
         /* LAB 4 TODO BEGIN (exercise 7) */
         /* Complete the following fields of shm, replace xxx with actual values */
-        // conn->shm.client_shm_uaddr = xxx;
-        // conn->shm.shm_size = xxx;
-        // conn->shm.shm_cap_in_client = xxx;
-        // conn->shm.shm_cap_in_server = xxx;
+        conn->shm.client_shm_uaddr = shm_addr_client;
+        conn->shm.shm_size = shm_size;
+        conn->shm.shm_cap_in_client = shm_cap_client;
+        conn->shm.shm_cap_in_server = shm_cap_server;
         /* LAB 4 TODO END (exercise 7) */
 
         lock_init(&conn->ownership);
@@ -381,14 +381,13 @@ static void ipc_thread_migrate_to_server(struct ipc_connection *conn,
          */
 
         /* Note: see how stack address and ip are get in sys_ipc_register_cb_return */
-        // arch_set_thread_stack(target, xxx);
-        // arch_set_thread_next_ip(target, xxx);
-
+        arch_set_thread_stack(target, handler_config->ipc_routine_stack);
+        arch_set_thread_next_ip(target, handler_config->ipc_routine_entry);
         /* see server_handler type in uapi/ipc.h */
-        // arch_set_thread_arg0(target, xxx);
-        // arch_set_thread_arg1(target, xxx);
-        // arch_set_thread_arg2(target, xxx);
-        // arch_set_thread_arg3(target, xxx);
+        arch_set_thread_arg0(target, shm_addr);
+        arch_set_thread_arg1(target, shm_size);
+        arch_set_thread_arg2(target, cap_num);
+        arch_set_thread_arg3(target, conn->client_badge);
         /* LAB 4 TODO END (exercise 7) */
 
         set_thread_arch_spec_state_ipc(target);
@@ -522,14 +521,13 @@ cap_t sys_register_client(cap_t server_cap, unsigned long shm_config_ptr)
         /* LAB 4 TODO BEGIN (exercise 7) */
         /* Set target thread SP/IP/arg, replace xxx with actual arguments */
         /* Note: see how stack address and ip are get in sys_register_server */
-        // arch_set_thread_stack(register_cb_thread, xxx);
-        // arch_set_thread_next_ip(register_cb_thread, xxx);
-
+        arch_set_thread_stack(register_cb_thread, register_cb_config->register_cb_stack);
+        arch_set_thread_next_ip(register_cb_thread, register_cb_config->register_cb_entry);
         /*
          * Note: see the parameter of register_cb function defined
          * in user/chcore-libc/musl-libc/src/chcore-port/ipc.c
          */
-        // arch_set_thread_arg0(register_cb_thread, xxx);
+        arch_set_thread_arg0(register_cb_thread, server_config->declared_ipc_routine_entry);
         /* LAB 4 TODO END (exercise 7) */
 
         obj_put(server);
@@ -654,6 +652,7 @@ unsigned long sys_ipc_call(cap_t conn_cap, unsigned int cap_num)
                 goto out_obj_put;
 
         if (cap_num != 0) {
+                // xiunian: wow! TODOTODO, send cap to the handler
                 r = ipc_send_cap(conn->server_handler_thread, cap_num);
                 if (r < 0)
                         goto out_release_lock;
@@ -723,7 +722,7 @@ int sys_ipc_return(unsigned long ret, unsigned int cap_num)
          *	  Then check if client is shadow
          *         -> No: set client to TS_EXIT and then sched
          *         -> Yes: return to client (it will recycle itself at next
-         *ipc_return)
+         *ipc_return in Step-1)
          *     -> No: return normally
          */
         if (client->thread_ctx->thread_exit_state == TE_EXITING) {
@@ -731,7 +730,7 @@ int sys_ipc_return(unsigned long ret, unsigned int cap_num)
 
                 /*
                  * Currently, a connection is assumed to belong to the client
-                 * process. So, it the client is exiting, then the connection is
+                 * process. So, if the client is exiting, then the connection is
                  * useless.
                  */
 
@@ -766,6 +765,7 @@ int sys_ipc_return(unsigned long ret, unsigned int cap_num)
                 }
         }
 
+        // xiunian: send back the cap
         if (cap_num != 0) {
                 int r = ipc_send_cap(conn->current_client_thread, cap_num);
                 if (r < 0)
@@ -891,7 +891,7 @@ int sys_ipc_register_cb_return(cap_t server_handler_thread_cap,
 
         /* LAB 4 TODO BEGIN (exercise 7) */
         /* Complete the server_shm_uaddr field of shm, replace xxx with the actual value */
-        // conn->shm.server_shm_uaddr = xxx;
+        conn->shm.server_shm_uaddr = server_shm_addr;
         /* LAB 4 TODO END (exercise 7) */
 
         conn->server_handler_thread = ipc_server_handler_thread;
